b4ed166a68f4ae6bd102c70c606d45df94bfae64
tjtjtjtj <nemunemutjtjtjtj@gmail.com>
2021-09-16 18:22:28 +0900

    tweat

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ import boto3
 client = boto3.client('glue')
 
 path = './metadata/docs/'
-output_file = '_column.md'
+output_file_suffix = '_column.md'
 
 for f in glob.glob(f'{path}*/*'):
-    if not os.path.basename(f).endswith(output_file):
+    if not os.path.basename(f).endswith(output_file_suffix):
         database_name = os.path.dirname(f).replace(path,'')
         table_name = os.path.splitext(os.path.basename(f))[0]
         res = client.get_table(DatabaseName=database_name, Name=table_name)
         columns = res['Table']['StorageDescriptor']['Columns']
 
-        with open(path + database_name + '/' + table_name + output_file, mode='w') as f:
+        with open(path + database_name + '/' + table_name + output_file_suffix, mode='w') as f:
             f.write('|Name|Comment|\n')
             f.write('|---|---|\n')
             for i in columns:
